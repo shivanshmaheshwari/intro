2ea7e7f0a8a4ded251359f3b65dc55510fde4d91
shivansh <shivanshmaheshwari99@gmail.com>
2022-02-06 16:57:30 +0530

    day 1

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Material(
+            child: Center(
+                child: Container(child: Text("welcome to my 1st app")))));
+  }
+}
